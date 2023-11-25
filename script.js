b8f616c92cae3a7d84959f33c9a3048722879e2e
karlinikhilkumar <135853242+karlinikhilkumar@users.noreply.github.com>
2023-11-25 19:29:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+ const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+
+function toggleMenu() {
+ 
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
